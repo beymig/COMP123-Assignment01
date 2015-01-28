f62de0931456502b0fc77fd2c099db7fdf3641d2
beymig <beymig@gmail.com>
2015-01-27 20:58:58 -0500

    Class, properties and methods created as required

---
+++ Assignment01/Hero.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    class Hero
+    {
+        
+        //Private Properties
+
+        private int strength = 0;
+        private int speed = 0;
+        private int health = 0;
+
+        //Public Properties
+        public string name;
+
+        //Constructor
+        public Hero(string name)
+        {
+            this.strength = 0;
+            this.speed = 0;
+            this.health = 0;
+            this.name = name;
+
+            this.generateAbilities();
+        }
+
+        private void generateAbilities()
+        {
+
+
+        }
+
+        public bool hitAttempt()
+        {
+
+
+        }
+
+        private bool hitAttempt()
+        {
+
+        }
+
+        private int hitDamage()
+        {
+
+        }
+
+        public void show ()
+        {
+
+
+        }
+    }
+}
