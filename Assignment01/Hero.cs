6be235d47593c0bd98bc0bd7ff7fc6ab173c0157
beymig <beymig@gmail.com>
2015-02-02 21:20:42 -0500

    Started Assignment 2, added Super Hero Class

--- Assignment01/Hero.cs
+++ Assignment01/Hero.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment01
+namespace Assignment2
 {
     class Hero
     {
--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -9,14 +9,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment01
+namespace Assignment2
 {
     class Program
     {
         //creates the new hero and calls the required methods
         static void Main(string[] args)
         {
-            Hero Superman = new Hero("Superman");
+            Hero Superman = new Hero("Superman");`
             Superman.show();
             Superman.fight();
             Console.WriteLine("Press any key to continue");
